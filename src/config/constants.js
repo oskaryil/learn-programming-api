31613a4c96981f338e9c91ea18d850524d2def4e
Oskar Yildiz <yildizoskar@gmail.com>
2018-10-13 12:06:54 +0200

    Added environment sensitive config

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,26 @@
 require('dotenv').config();
 
-module.exports = (env) => {
+const env = process.env.NODE_ENV;
 
+const defaultConfig = {
+  APP_NAME: process.env.APP_NAME || 'Default app',
+  PORT: process.env.PORT || 4000
 };
+
+const devConfig = {};
+const stagingConfig = {};
+const prodConfig = {};
+
+module.exports = envConfig(env);
+function envConfig(env) {
+  switch(env) {
+    case 'development':
+      return Object.assign(defaultConfig, devConfig);
+    case 'staging':
+      return Object.assign(defaultConfig, stagingConfig);
+    case 'production':
+      return Object.assign(defaultConfig, prodConfig);
+    default:
+      return Object.assign(defaultConfig, prodConfig);
+  }
+}
