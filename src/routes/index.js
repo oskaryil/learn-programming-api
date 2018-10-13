9991d98246ff52c7b0ae59a3a803719b0ef26ddd
Oskar Yildiz <yildizoskar@gmail.com>
2018-10-13 15:03:43 +0200

    Added invite route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 const Router = require('koa-router');
 
+const inviteController = require('../controllers/invite.controller');
+
 const router = new Router();
 
 router.get('/', (ctx, next) => {
   ctx.body = 'Learn Programming API'
-});
+})
+router.post('/invite', inviteController.sendSlackInvite);
 
 module.exports = router;
