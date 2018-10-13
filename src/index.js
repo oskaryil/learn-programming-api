98ae090848eb10bcb1c7e0079b4b6b78d0e9d3a9
Oskar Yildiz <yildizoskar@gmail.com>
2018-10-13 12:07:56 +0200

    Basic boilerplate setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const Koa = require('koa');
 
-const app = Koa();
+const constants = require('./config/constants');
+
+const app = new Koa();
+const middlewares = require('./config/middlewares')(app);
 
 app.listen(constants.PORT, (err) => {
   if(err) throw err;
-  console.log(`App listening on port ${constans.PORT}`);
+  console.log(`${constants.APP_NAME} listening on port ${constants.PORT}`);
 });
